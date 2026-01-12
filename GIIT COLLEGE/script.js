fe33e8c0a46a79eb0f3f7c5c4586d4b35a02a10b
kamna162 <kamnabhadauriya01@gmail.com>
2026-01-12 17:22:27 +0530

    first commit

---
+++ GIIT COLLEGE/script.js
@@ -0,0 +1,77 @@
+new Swiper(".wrapper", {
+  loop: true,
+  spaceBetween: 30,
+  // Autoplay
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    // 1288: {
+    //   slidesPerView: 4,
+    // },
+  },
+});
+
+
+
+new Swiper(".wrappers", {
+  loop: true,
+  spaceBetween: 30,
+  // Autoplay
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1288: {
+      slidesPerView: 4,
+    },
+  },
+});
+ 
+  
